0c76173852eddece07e3801f19c9f018b5330cc2
zagpro1408 <zagorskybobuyc@gmail.com>
2017-04-15 01:03:52 +0300

    6

---
+++ 6.rb
@@ -0,0 +1,19 @@
+# Дан целочисленный массив.
+# Преобразовать его, прибавив к четным числам последний элемент.
+# Первый и последний элементы массива не изменять.
+
+require "./methods.rb"
+# Создадим массив
+array = ArrayForTast.new
+array = array.create_array_for_task 100
+
+p array
+
+1.upto(array.size-2) do |n|
+  if array[n].even?
+    array[n] += array[-1]
+  end
+end
+
+puts
+p array

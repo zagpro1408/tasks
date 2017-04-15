11dedf29ba80cc167737fa873c7f8b58e60a6a9a
zagpro1408 <zagorskybobuyc@gmail.com>
2017-04-15 15:06:46 +0300

    8

---
+++ 8.rb
@@ -0,0 +1,19 @@
+# Дан целочисленный массив.
+# Преобразовать его, прибавив к нечетным числам первый элемент.
+# Первый и последний элементы массива не изменять.
+
+require "./methods.rb"
+# Создадим массив
+array = ArrayForTast.new
+array = array.create_array_for_task 100
+
+p array
+
+1.upto(array.size-2) do |n|
+  if array[n].odd?
+    array[n] += array[0]
+  end
+end
+
+puts
+p array

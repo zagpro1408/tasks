c5d342ac55ba6424a0cae79c4946c0a82ea548c6
zagpro1408 <zagorskybobuyc@gmail.com>
2017-04-19 18:33:13 +0300

    26

---
+++ 26.rb
@@ -0,0 +1,20 @@
+# Дан целочисленный массив.
+# Преобразовать его, вставив перед каждым отрицательным
+# элементом нулевой элемент.
+
+srand 123
+array = Array.new(20) { rand -100..100 }
+new_array = []
+p array
+
+array.each do |elem|
+  if elem < 0
+    new_array << 0
+    new_array << elem
+  else
+    new_array << elem
+  end
+end
+
+array = new_array
+p array
